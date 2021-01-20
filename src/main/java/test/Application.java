df368e2a90e32eac632f26a410a8f09ffa181205
emanueledomingo <emanuele.domingo@hotmail.it>
2021-01-20 17:24:19 +0100

    created first spring application

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; //Autoconfiguration
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
