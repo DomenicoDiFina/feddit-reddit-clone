f4bc524b3c6e30e797abe9d82bc9ce870a94b5ea
emanueledomingo <emanuele.domingo@hotmail.it>
2021-01-21 16:06:54 +0100

    Added index page

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import test.Post;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/sign-up")
+    public String getMyArticle(Model model) {
+        return "sign-up";
+    }
+}
