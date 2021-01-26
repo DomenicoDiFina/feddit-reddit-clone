b5289a5362e25c087f811cc7ae59856b9c590e97
gristsal97 <gristsal97@gmail.com>
2021-01-26 16:23:54 +0100

    update sign-up.html and functions.js

--- src/main/resources/static/js/functions.js
+++ src/main/resources/static/js/functions.js
@@ -1,3 +1,22 @@
 function showChangePasswordForm() {
   document.getElementById("changePasswordForm").classList.toggle("hidden");
 }
+
+function checkPassword() {
+   if (document.getElementById('password').value ==
+     document.getElementById('confirm_password').value) {
+     document.getElementById('confirm_password').style.borderColor = "green"
+     document.getElementById('password').style.borderColor = "black"
+     return true
+   } else {
+     document.getElementById('confirm_password').style.borderColor = "black"
+     return false
+   }
+ }
+
+ function checkErrorPassword(){
+    if (!checkPassword()) {
+        document.getElementById('errorPassword').innerHTML = "passwords are different";
+        document.getElementById('password').style.borderColor = "red"
+    }
+ }
