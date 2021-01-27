48ef9ad16b557cf443a253ba2bf0e7e52c0b3270
Dario Curreri <dariocurr@gmail.com>
2021-01-27 10:26:17 +0100

    from query to java code

--- src/main/java/feddit/repositories/UserRepository.java
+++ src/main/java/feddit/repositories/UserRepository.java
@@ -1,14 +1,20 @@
 package feddit.repositories;
 
 import feddit.model.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    @Query("SELECT u FROM User u WHERE u.username = ?1")
-    User findByUsername(String username);
+    default Optional<User> findByUsername(String username) {
+        return StreamSupport
+                .stream(this.findAll().spliterator(), false)
+                .filter(user -> user.getUsername().equals(username))
+                .findAny();
+    }
 
 }
--- src/main/java/feddit/services/AuthService.java
+++ src/main/java/feddit/services/AuthService.java
@@ -2,7 +2,6 @@ package feddit.services;
 
 
 import feddit.model.User;
-import feddit.repositories.UserRepository;
 import feddit.security.FedditUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,11 +12,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 public class AuthService implements UserDetailsService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+        User user = this.userService.findByUsername(username);
         if(user == null) {
             throw new UsernameNotFoundException("User not found");
         }
