8dc302c952d350e88a4dcd7f6bc2b68e56105fff
emanueledomingo <emanuele.domingo@hotmail.it>
2021-01-27 12:05:48 +0100

    Query refactoring

--- src/main/java/feddit/repositories/UserRepository.java
+++ src/main/java/feddit/repositories/UserRepository.java
@@ -1,20 +1,22 @@
 package feddit.repositories;
 
 import feddit.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    default Optional<User> findByUsername(String username) {
+    default User findByUsername(String username) {
         return StreamSupport
                 .stream(this.findAll().spliterator(), false)
                 .filter(user -> user.getUsername().equals(username))
-                .findAny();
+                .findAny().orElseThrow( () -> new UsernameNotFoundException("User not found"));
     }
 
 }
