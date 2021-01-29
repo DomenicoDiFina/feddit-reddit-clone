2ed0fe8fa4c6e64342b39108f84fac061ce29e45
Dario Curreri <dariocurr@gmail.com>
2021-01-29 11:36:25 +0100

    create log file

--- src/main/java/feddit/logger/Logger.java
+++ src/main/java/feddit/logger/Logger.java
@@ -5,15 +5,31 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @Aspect
 public class Logger {
+
+    private final Path path = Path.of("src/main/resources/log.txt");
+
+    public Logger() {}
 
     @Pointcut("execution(public * feddit.controllers.UserController.processSignUp*(..))")
     private void createUserPointcut() {}
 
     @After("createUserPointcut()")
     private void afterCreateUser(JoinPoint joinPoint) {
-        System.out.println("Create user calling " + joinPoint.getSignature().getName() + " " + joinPoint.getArgs()[0]);
+        this.writeToFile("Create user calling " + joinPoint.getSignature().getName());
+    }
+
+    private void writeToFile(String string) {
+        try {
+            Files.writeString(path, string);
+        } catch (IOException e) {
+            System.out.println("Error: can't write to file!");
+        }
     }
 
 }
