14f1cbdc50be4bc1dcab9b94dbd1a7dfacf3d02b
Domenico Di Fina <difinadomenico98@gmail.com>
2021-01-29 10:29:43 +0100

    added votes in comments

--- src/main/java/feddit/controllers/VoteController.java
+++ src/main/java/feddit/controllers/VoteController.java
@@ -1,9 +1,11 @@
 package feddit.controllers;
 
+import feddit.model.Comment;
 import feddit.model.Post;
 import feddit.model.User;
 import feddit.model.Vote;
 import feddit.security.FedditUserDetails;
+import feddit.services.CommentService;
 import feddit.services.PostService;
 import feddit.services.UserService;
 import feddit.services.VoteService;
@@ -31,8 +33,11 @@ public class VoteController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private CommentService commentService;
+
     @PostMapping("/votePost/{id}")
-    public String processVote(@AuthenticationPrincipal FedditUserDetails userDetails,
+    public String processPostVote(@AuthenticationPrincipal FedditUserDetails userDetails,
                                        RedirectAttributes redirectAttributes,
                                       @PathVariable long id, Vote vote, Model model) throws Exception {
         User user = userService.findByUsername(userDetails.getUsername());
@@ -87,6 +92,72 @@ public class VoteController {
         if(postService.save(post)) {
         } else {
             redirectAttributes.addFlashAttribute("postError", "An error occured.");
+        }
+
+        return "redirect:/";
+    }
+
+
+    @PostMapping("/voteComment/{id}")
+    public String processCommentVote(@AuthenticationPrincipal FedditUserDetails userDetails,
+                              RedirectAttributes redirectAttributes,
+                              @PathVariable long id, Vote vote, Model model) throws Exception {
+        System.out.println("Ciao, sono qui");
+        User user = userService.findByUsername(userDetails.getUsername());
+        Comment comment = commentService.findById(id);
+        Optional<Vote> optVote = voteService.findByCommentAndUser(user, comment);
+
+        if (optVote.isPresent() &&
+                optVote.get().getType()
+                        .equals(vote.getType())) {
+            voteService.remove(optVote.get());
+            System.out.println("VOTO UGUALE E GIA PRESENTE");
+
+            if(vote.getType().equals("UPVOTE"))
+                comment.setUpVotes(comment.getUpVotes() - 1);
+            else
+                comment.setDownVotes(comment.getDownVotes() - 1);
+
+        }
+        else if (optVote.isPresent() && !optVote.get().getType().equals(vote.getType())){
+            voteService.remove(optVote.get());
+            System.out.println("VOTO DIVERSO E GIA PRESENTE");
+
+            if(vote.getType().equals("UPVOTE"))
+                comment.setUpVotes(comment.getUpVotes() + 2);
+            else
+                comment.setDownVotes(comment.getDownVotes() + 2);
+
+            vote.setComment(comment);
+            vote.setUser(user);
+
+            if (voteService.save(vote)) {
+                redirectAttributes.addFlashAttribute("voteAdded", "Vote added successfully");
+            } else {
+                redirectAttributes.addFlashAttribute("voteError", "An error occured.");
+            }
+        }
+        else {
+            System.out.println("VOTO NON PRESENTE");
+            if("UPVOTE".equals(vote.getType())) {
+                comment.setUpVotes(comment.getUpVotes() + 1);
+            } else if("DOWNVOTE".equals(vote.getType())){
+                comment.setDownVotes(comment.getDownVotes() + 1);
+            }
+
+            vote.setComment(comment);
+            vote.setUser(user);
+
+            if (voteService.save(vote)) {
+                redirectAttributes.addFlashAttribute("voteAdded", "Vote added successfully");
+            } else {
+                redirectAttributes.addFlashAttribute("voteError", "An error occured.");
+            }
+        }
+
+        if(commentService.save(comment)) {
+        } else {
+            redirectAttributes.addFlashAttribute("commentError", "An error occured.");
         }
 
         return "redirect:/";
