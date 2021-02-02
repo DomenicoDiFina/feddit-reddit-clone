8b1df6bda4f9834e63db43a38fa734a4dd1bb495
arnaud <arnaud.vanspauwen@student.ucll.be>
2021-02-02 13:43:22 +0100

    notempty

--- src/main/java/feddit/model/Role.java
+++ src/main/java/feddit/model/Role.java
@@ -3,16 +3,17 @@ package feddit.model;
 import feddit.model.hierarchy.DatabaseObject;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "roles")
 public class Role extends DatabaseObject {
 
+    @NotEmpty
     @Column(name = "description")
     private String description;
 
-    public Role() {
-    }
+    public Role() {}
 
     @Override
     public String getClazz() {
