eb3e82c34dcf9ce81f383924cde0f893f73981e0
Dario Curreri <dariocurr@gmail.com>
2021-02-02 23:02:36 +0100

    remove unused methods

--- src/main/java/feddit/repositories/VoteRepository.java
+++ src/main/java/feddit/repositories/VoteRepository.java
@@ -1,31 +1,10 @@
 package feddit.repositories;
 
-import feddit.model.Comment;
-import feddit.model.Post;
-import feddit.model.User;
 import feddit.model.Vote;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
-import java.util.stream.StreamSupport;
 
 @Repository
 public interface VoteRepository extends CrudRepository<Vote, Long> {
-
-    default Optional<Vote> findByPostAndUser(User user, Post post) {
-        if (this.findAll() == null) return Optional.empty();
-        return StreamSupport
-                .stream(this.findAll().spliterator(), false)
-                .filter(vote -> vote.getUser().equals(user) && vote.getPost() != null && vote.getPost().equals(post))
-                .findAny();
-    }
-
-    default Optional<Vote> findByCommentAndUser(User user, Comment comment) {
-        if (this.findAll() == null) return Optional.empty();
-        return StreamSupport
-                .stream(this.findAll().spliterator(), false)
-                .filter(vote -> vote.getUser().equals(user) && vote.getComment() != null && vote.getComment().equals(comment))
-                .findAny();
-    }
 
 }
--- src/main/java/feddit/services/VoteService.java
+++ src/main/java/feddit/services/VoteService.java
@@ -1,29 +1,16 @@
 package feddit.services;
 
-import feddit.model.Comment;
-import feddit.model.Post;
-import feddit.model.User;
 import feddit.model.Vote;
 import feddit.repositories.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class VoteService implements ForumService<Vote> {
 
     @Autowired
     private VoteRepository voteRepository;
-
-    public Optional<Vote> findByPostAndUser(User user, Post post) {
-        return this.voteRepository.findByPostAndUser(user, post);
-    }
-
-    public Optional<Vote> findByCommentAndUser(User user, Comment comment) {
-        return this.voteRepository.findByCommentAndUser(user, comment);
-    }
 
     @Override
     public CrudRepository<Vote, Long> getCrudRepository() {
